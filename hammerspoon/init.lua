e5e71f20ac56856feddbcddad11d72467d777eff
S. Hyeon <loudclr@gmail.com>
2020-06-08 15:37:06 +0900

    use system german/french input method

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -7,51 +7,37 @@ do
 	-- key modification from option(right) to f13 by karabiner-elements is required.
 
 	local IME = {
-		system_colemak = "com.apple.keylayout.Colemak",
 		kotoeri_romaji = "com.apple.inputmethod.Kotoeri.Roman",
 		kotoeri_hiragana = "com.apple.inputmethod.Kotoeri.Japanese",
-		gureum_colemak = "org.youknowone.inputmethod.Gureum.colemak",
+		system_colemak = "com.apple.keylayout.Colemak",
 		gureum_han390 = "org.youknowone.inputmethod.Gureum.han390",
-
-		ukelele_prefix = "org.sil.ukelele.keyboardlayout.interlingualcolemak",
-
-		russian = ".colemak-russian",
-		french = ".colemak-french",
 	} 
 
 	local toggleAlphabet = function()
 		local currentIME = hs.keycodes.currentSourceID()
 		local nextIME = ""
-		local nextIME_cor = ""
-		if currentIME == IME.gureum_colemak then
+		if currentIME == IME.system_colemak then
 			nextIME = IME.gureum_han390
 		elseif currentIME == IME.gureum_han390 then
-			nextIME = IME.gureum_colemak
+			nextIME = IME.system_colemak
 		elseif currentIME == IME.kotoeri_romaji then
 			nextIME = IME.kotoeri_hiragana
 		elseif currentIME == IME.kotoeri_hiragana then
 			nextIME = IME.kotoeri_romaji
-		elseif currentIME == IME.system_colemak then
-			nextIME = IME.ukelele_prefix..IME.russian
-			nextIME_cor = IME.ukelele_prefix..".keylayout"..IME.russian
-		elseif string.sub(currentIME, -string.len(IME.russian)) == IME.russian then
-			nextIME = IME.system_colemak
 		else
 			nextIME = currentIME()
 		end
 
-		if not hs.keycodes.currentSourceID(nextIME) then
-			hs.keycodes.currentSourceID(nextIME_cor)
-		end
+		hs.keycodes.currentSourceID(nextIME)
 	end
 
 	local escVim = function()
 		local currentIME = hs.keycodes.currentSourceID()
 		if currentIME == IME.gureum_han390 then
-			hs.keycodes.currentSourceID(IME.gureum_colemak)
+			hs.keycodes.currentSourceID(IME.system_colemak)
 		elseif currentIME == IME.kotoeri_hiragana then
 			hs.keycodes.currentSourceID(IME.kotoeri_romaji)
-		elseif string.sub(currentIME, -string.len(IME.russian)) == IME.russian then
+		else
 			hs.keycodes.currentSourceID(IME.system_colemak)
 		end
 		hs.eventtap.keyStroke({}, 'escape')
@@ -59,29 +45,6 @@ do
 
 	hs.hotkey.bind('','f2', function() hs.keycodes.currentSourceID(IME.gureum_han390) end)
 	hs.hotkey.bind('','f3', function() hs.keycodes.currentSourceID(IME.kotoeri_hiragana) end)
-	hs.hotkey.bind('','f4', function() 
-		if not hs.keycodes.currentSourceID(IME.ukelele_prefix..IME.french) then
-			hs.keycodes.currentSourceID(IME.ukelele_prefix..".keylayout"..IME.french)
-		end
-	end)
-	hs.hotkey.bind('','f5', function() 
-		if not hs.keycodes.currentSourceID(IME.ukelele_prefix..IME.russian) then
-			hs.keycodes.currentSourceID(IME.ukelele_prefix..".keylayout"..IME.russian)
-		end
-	end)
-
-	hs.hotkey.bind('shift','f2', function() hs.keycodes.currentSourceID(IME.gureum_han390) end)
-	hs.hotkey.bind('shift','f3', function() hs.keycodes.currentSourceID(IME.kotoeri_hiragana) end)
-	hs.hotkey.bind('shift','f4', function() 
-		if not hs.keycodes.currentSourceID(IME.ukelele_prefix..IME.french) then
-			hs.keycodes.currentSourceID(IME.ukelele_prefix..".keylayout"..IME.french)
-		end
-	end)
-	hs.hotkey.bind('shift','f5', function() 
-		if not hs.keycodes.currentSourceID(IME.ukelele_prefix..IME.russian) then
-			hs.keycodes.currentSourceID(IME.ukelele_prefix..".keylayout"..IME.russian)
-		end
-	end)
 
 	hs.hotkey.bind('control','[',escVim)
 	hs.hotkey.bind('','f16',escVim)
